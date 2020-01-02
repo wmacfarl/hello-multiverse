798bbf01084c98778ada7a62da0356d65f51bf1a
wmacfarl <wmacfarl@gmail.com>
2020-01-01 19:48:16 -0500

    Fix some bugs in color palette.

--- src/app/tile-editor/tile-editor.component.ts
+++ src/app/tile-editor/tile-editor.component.ts
@@ -30,7 +30,11 @@ export class TileEditorComponent implements OnInit {
   }
 
   private sketch = (p: any) => {
+    
+
     p.setup = () => {
+      p.selectedColor = [255,255,255];
+      p.pMouseIsPressed = p.mouseIsPressed;
       p.pixelGrid = new PixelGrid(this.tileDimension,this.tileDimension);
       p.colorPalette = new ColorPalette(16);
       let canvas = p.createCanvas(this.pixelCanvasSize*1.5, this.pixelCanvasSize).parent('tile-editor-div');
@@ -41,24 +45,48 @@ export class TileEditorComponent implements OnInit {
       p.fill(0);
       p.drawPixelGrid(p.pixelGrid)
       p.drawColorPalette(p.colorPalette);
+      p.pMouseIsPressed = p.mouseIsPressed;
     };
 
     p.drawColorPalette = (colorPalette: ColorPalette) => {
       for(let i = 0; i <colorPalette.colorCount; i++ ){
         p.stroke(255);
+        let x = this.pixelCanvasSize*1.5-this.pixelDrawSize*2;
+        let y = this.pixelDrawSize*i;
+        let w = this.pixelDrawSize*2;
+        let h = this.pixelDrawSize;
+
         p.fill(colorPalette.paletteColors[i][0],
                  colorPalette.paletteColors[i][1],
                  colorPalette.paletteColors[i][2]);
-        p.rect(this.pixelCanvasSize*1.5-this.pixelDrawSize*2, this.pixelDrawSize*i, this.pixelDrawSize*2,this.pixelDrawSize);
+        p.rect(x, y,w ,h);
+
+        if(p.mouseX >= x && p.mouseY >= y 
+          && p.mouseX < x+w && p.mouseY < y+h){
+         if(p.mouseIsPressed && !p.pMouseIsPressed){
+          p.selectedColor = colorPalette.paletteColors[i];
+         }
+       }
       }
     }
 
     p.drawPixelGrid = (pixelGrid: PixelGrid)=>{
-      for (let x = 0; x < pixelGrid.width; x++){
-        for (let y = 0; y < pixelGrid.height; y++){
+      for (let xIndex = 0; xIndex < pixelGrid.width; xIndex++){
+        for (let yIndex = 0; yIndex < pixelGrid.height; yIndex++){
+          let x = xIndex*this.pixelDrawSize;
+          let y = yIndex*this.pixelDrawSize;
+          let w = this.pixelDrawSize;
+          let h = this.pixelDrawSize;
+
+          if(p.mouseX >= x && p.mouseY >= y 
+            && p.mouseX < x+w && p.mouseY < y+h &&
+            p.mouseIsPressed && !p.pMouseIsPressed){
+            pixelGrid.pixels[xIndex][yIndex] = p.color(p.selectedColor[0], p.selectedColor[1], p.selectedColor[2]);
+           }
+          
           p.stroke(255);
-          p.fill(pixelGrid.pixels[x][y]);
-          p.rect(x*this.pixelDrawSize, y*this.pixelDrawSize, this.pixelDrawSize, this.pixelDrawSize);
+          p.fill(pixelGrid.pixels[xIndex][yIndex]);
+          p.rect(x,y,w,h);
         }
       }
     }
