4be0f115b00a40f34e0acfcc6f2d96011cd0edfd
wmacfarl <wmacfarl@gmail.com>
2020-02-02 22:49:01 -0500

    Display active page with navbar
    app.js: add app_modes array
    views\navbar.js: refactor to use app_modes array and set "active" based on mode
    views\app-modes\sprite.ejs: remove redundant header

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 var bodyParser = require("body-parser");
 
 var app = express();
+var app_modes = ["map", "screen", "code", "sprite", "tile", "music"]
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -20,7 +21,7 @@ app.get("/", function (request, response){
 
 
 app.get("/mode/:mode", function (request, response){
-    response.render("index", {app_mode: request.params.mode});
+    response.render("index", {app_mode: request.params.mode, app_modes: app_modes});
 });
 
 http.createServer(app).listen(process.env.PORT || 3000)
